3bf2135b8fa5a7e50cca154eb201a536db8be076
mike256bit <mike.toth@gmail.com>
2019-01-25 20:42:30 -0500

    Initial project setup commit.

---
+++ HelloMVC/HelloMVC/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello MinVoiCe");
+        }
+    }
+}
